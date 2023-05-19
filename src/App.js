e3f0d11c5ce2e02404af874db1e3b5d6d38ce12c
iewher <goshagang@mail.ru>
2023-05-19 23:07:52 +0500

    added button and styled input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './components/pages/page-home/Home';
+import { Home } from './components/pages/page-home/Home';
 import './style/style.css'
 
 function App() {
--- src/components/pages/page-home/Home.jsx
+++ src/components/pages/page-home/Home.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import './style/home-style.css'
+import {BsSearch} from 'react-icons/bs'
 import background from './source/background.mp4'
 
-export default function Home() {
+const Create_input = () => {
+  return (
+    <input placeholder='Выберите фильм' />
+  )
+}
+
+const Create_button = () => {
+  return (
+    <div className='home-button'>
+      <button>
+        <BsSearch />
+      </button>
+      </div>
+  )
+}
+
+export const Home = () => {
   return (
     <div className='home'>
         <video src={background} autoplay="true" loop="true" muted="true"/>
         <div className='home-input'>
-          <input placeholder='Выберите фильм' />
+          <Create_input />
+          <Create_button />
         </div>
     </div>
   )
