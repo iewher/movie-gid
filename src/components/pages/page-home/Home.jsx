abed8b9dbfcf2b481a85f41c84152504244d6d28
iewher <goshagang@mail.ru>
2023-05-20 02:42:41 +0500

    added responsive resolution

--- src/components/pages/page-home/Home.jsx
+++ src/components/pages/page-home/Home.jsx
@@ -1,32 +1,38 @@
 import React from 'react'
 import './style/home-style.css'
-import {BsSearch} from 'react-icons/bs'
+import { BsSearch } from 'react-icons/bs'
 import background from './source/background.mp4'
 
-const Create_input = () => {
+const CreateInput = () => {
   return (
-    <input placeholder='Выберите фильм' />
+    <div className='home-input'>
+      <input placeholder='Выберите фильм' />
+    </div>
   )
 }
 
-const Create_button = () => {
+const CreateButton = () => {
   return (
     <div className='home-button'>
       <button>
         <BsSearch />
       </button>
-      </div>
+    </div>
   )
 }
 
 export const Home = () => {
   return (
     <div className='home'>
-        <video src={background} autoplay="true" loop="true" muted="true"/>
-        <div className='home-input'>
-          <Create_input />
-          <Create_button />
+
+      <div className='video-container'>
+        <video src={background} autoPlay loop muted />
+        <p>asfafsasfafasfasf</p>
+        <div className='content'>
+          <CreateInput />
+          <CreateButton />
         </div>
+      </div>
     </div>
   )
 }
