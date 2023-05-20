90c956fdbb5869e233964c6b76590754f1ed25e5
iewher <goshagang@mail.ru>
2023-05-20 16:16:57 +0500

    added error message if request === null

--- src/components/pages/page-home/Home.jsx
+++ src/components/pages/page-home/Home.jsx
@@ -1,35 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style/home-style.css'
 import { BsSearch } from 'react-icons/bs'
+import { CgSmileSad } from 'react-icons/cg'
 import background from './source/background.mp4'
 
-const CreateInput = () => {
+const CreateInput = ({ onFilmsChange }) => {
+  const handleFilms = (event) => {
+    onFilmsChange(event.target.value);
+  }
+
   return (
     <div className='home-input'>
-      <input placeholder='Выберите фильм' />
+      <input 
+      placeholder='Выберите фильм' 
+      onChange={handleFilms}/>
     </div>
   )
 }
 
-const CreateButton = () => {
+const CreateButton = ({ onClick }) => {
   return (
     <div className='home-button'>
-      <button>
+      <button onClick={onClick}>
         <BsSearch />
       </button>
     </div>
   )
 }
 
+const ReturnError = () => {
+  return (
+    <div className='error'>
+      <h2>Извините, но вы не сделали запрос</h2>
+    </div>
+  )
+}
+
 export const Home = () => {
+  const [films, setFilms] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = () => {
+    if (films !== '') {
+      localStorage.setItem('film', films)
+    } else {
+      setError(true);
+    }
+  }
+
   return (
     <div className='home'>
       <div className='video-container'>
         <video src={background} autoPlay loop muted />
+        <h1>Ищи информацию про свои любимые фильмы</h1>
         <div className='content'>
-          <CreateInput />
-          <CreateButton />
+          <CreateInput onFilmsChange={setFilms} />
+          <CreateButton onClick={handleSubmit} />
         </div>
+        {error && <ReturnError />}
       </div>
     </div>
   )
