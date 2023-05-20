e3c3a9f1b8a44324aacbbbb5692c6c8e0b4924bb
iewher <goshagang@mail.ru>
2023-05-20 20:29:28 +0500

    added react-router and craeted second page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Home } from './components/pages/page-home/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './style/style.css'
+
+import { Home } from './components/pages/page-home/Home';
+import { Info } from './components/pages/page-info/Info';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route parh='/info-page' element={<Info />}/>
+        </Routes>
+        <Home />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/page-info/Info.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style/info-style.css'
+
+export const Info = () => {
+    return (
+        <div>
+            123
+        </div>
+    )
+}
