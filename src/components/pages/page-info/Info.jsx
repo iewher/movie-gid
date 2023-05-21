f904f62e6401b29211ab8a85400a94c213b4d032
iewher <goshagang@mail.ru>
2023-05-22 01:04:36 +0500

    added fetch request

--- src/components/pages/page-info/Info.jsx
+++ src/components/pages/page-info/Info.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './style/info-style.css'
 
+
+
 export const Info = () => {
+    useEffect(() => {
+        fetchData();
+    })
+
+    const fetchData = () => {
+        const parsed_title = localStorage.getItem('film');
+        const title = JSON.parse(parsed_title);
+        const API_KEY = 'aa1985f4';
+        const API_URL = `https://www.omdbapi.com/?t=${title}&plot=full&apikey=` + API_KEY;
+        fetch(API_URL)
+        .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                return data;
+            })
+            .catch((error) => {
+                console.error(error.message)
+            })
+    }
+
     return (
         <div>
-            afasf
+            
         </div>
     )
 }
